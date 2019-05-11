3465019323e3f567aac24a9966e028ac08e87bb7
cyrill <cyrillmeyer96@gmail.com>
2019-05-11 16:07:13 +0200

    - list patients - click on patient in list and open in edit patient

--- MhcPms/src/main/java/ch/bfh/red/ui/views/EditPatientView.java
+++ MhcPms/src/main/java/ch/bfh/red/ui/views/EditPatientView.java
@@ -4,15 +4,17 @@ import ch.bfh.red.MainLayout;
 import ch.bfh.red.backend.models.Address;
 import ch.bfh.red.backend.models.Patient;
 import ch.bfh.red.ui.encoders.IntegerToStringEncoder;
-import ch.bfh.red.ui.encoders.LongToStringEncoder;
+import ch.bfh.red.ui.presenters.PatientPresenter;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Input;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.polymertemplate.EventHandler;
 import com.vaadin.flow.component.polymertemplate.Id;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.router.BeforeEvent;
+import com.vaadin.flow.router.HasUrlParameter;
+import com.vaadin.flow.router.OptionalParameter;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.templatemodel.Encode;
 import com.vaadin.flow.templatemodel.Exclude;
@@ -24,11 +26,21 @@ import java.util.List;
 @Route(value = "editpatient", layout = MainLayout.class)
 @Tag("editpatient-element")
 @HtmlImport("frontend://src/views/person/editPatient.html")
-public class EditPatientView extends PolymerTemplate<EditPatientView.EditPatientModel> implements View<EditPatientView.EditPatientViewListener>{
+public class EditPatientView extends PolymerTemplate<EditPatientView.EditPatientModel> implements View<EditPatientView.EditPatientViewListener>, HasUrlParameter<String> {
     List<EditPatientViewListener> listeners = new ArrayList<>();
-
     @Id("header")
     private H2 header;
+
+    @Override
+    public void setParameter(BeforeEvent beforeEvent, @OptionalParameter String s) {
+        if(s==null ||s.isEmpty()){
+            getModel().setPatient(new Patient());
+        }else{
+            System.out.println("find patient with id: " + s + " on db and set on model");
+            Patient p = new Patient("cyrill","meyer",new Address()); //can be replaced with found patient
+            getModel().setPatient(p);
+        }
+    }
 
     /** View Model Interface **/
     public interface EditPatientModel extends TemplateModel {
@@ -47,10 +59,8 @@ public class EditPatientView extends PolymerTemplate<EditPatientView.EditPatient
     }
 
     public EditPatientView(){
+        new PatientPresenter(this);
         header.setText("Edit Patient");
-        Patient p = new Patient("cyrill","meyer",new Address());
-        getModel().setPatient(p);
-
     }
 
     @EventHandler
---
+++ MhcPms/src/main/java/ch/bfh/red/ui/views/ListPatientView.java
@@ -0,0 +1,63 @@
+package ch.bfh.red.ui.views;
+
+import ch.bfh.red.MainLayout;
+import ch.bfh.red.backend.models.Address;
+import ch.bfh.red.backend.models.Patient;
+import ch.bfh.red.ui.encoders.LongToStringEncoder;
+import ch.bfh.red.ui.presenters.PatientPresenter;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.polymertemplate.EventHandler;
+import com.vaadin.flow.component.polymertemplate.Id;
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.component.polymertemplate.RepeatIndex;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.templatemodel.Encode;
+import com.vaadin.flow.templatemodel.TemplateModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Route(value = "listpatient", layout = MainLayout.class)
+@Tag("listpatient-element")
+@HtmlImport("frontend://src/views/person/listPatient.html")
+public class ListPatientView extends PolymerTemplate<ListPatientView.ListPatientModel> implements View<ListPatientView.ListPatientViewListener>{
+    List<ListPatientViewListener> listeners = new ArrayList<>();
+    private List<Patient> patientList = new ArrayList<>();
+
+    @Id("header")
+    private H2 header;
+
+    public interface ListPatientViewListener{
+
+    }
+
+    public interface ListPatientModel extends TemplateModel {
+        @Encode(value = LongToStringEncoder.class, path = "id")
+        void setPatienten(List<Patient> patienten);
+        List<Patient> getPatienten();
+    }
+
+    @Override
+    public void addListener(ListPatientViewListener listener) {
+        listeners.add(listener);
+    }
+
+    ListPatientView(){
+        new PatientPresenter(this);
+        header.setText("List Patient");
+        patientList.add(new Patient(1,"cyrill", "meyer", new Address("bethlehem 7", 3185, "schmitten")));
+        patientList.add(new Patient(2, "ueli", "kramer", new Address("thunstrasse 3", 2400, "thun")));
+        getModel().setPatienten(patientList);
+    }
+
+    @EventHandler
+    public void onClick(@RepeatIndex int itemIndex) {
+        String param = patientList.get(itemIndex).getId()+"";
+        UI.getCurrent().navigate(EditPatientView.class, param);
+    }
+
+
+}
