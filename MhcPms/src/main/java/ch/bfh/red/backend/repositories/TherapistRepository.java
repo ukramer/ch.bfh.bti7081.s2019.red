c5fc689913d75c19267c7cf0fb5f32c485980db7
Marc Hutzli <m.hutzli@hotmail.com>
2019-05-11 23:51:00 +0200

    Revert "Feature/2"

--- MhcPms/src/main/java/ch/bfh/red/backend/repositories/TherapistRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.bfh.red.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ch.bfh.red.backend.models.Therapist;
-
-public interface TherapistRepository extends JpaRepository<Therapist, Long> {
-}
--- MhcPms/src/main/java/ch/bfh/red/ui/views/View.java
+++
@@ -1,5 +0,0 @@
-package ch.bfh.red.ui.views;
-
-public interface View<Listener> {
-    void addListener(Listener listener);
-}
