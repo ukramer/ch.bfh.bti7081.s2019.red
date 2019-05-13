388f922e93c0aee501480f351da084bc42444c89
cyrill <cyrillmeyer96@gmail.com>
2019-05-13 20:11:47 +0200

    onPatientClick and search in patientPresenter

--- MhcPms/src/main/java/ch/bfh/red/ui/presenters/PatientPresenter.java
+++ MhcPms/src/main/java/ch/bfh/red/ui/presenters/PatientPresenter.java
@@ -2,20 +2,36 @@ package ch.bfh.red.ui.presenters;
 
 import ch.bfh.red.ui.views.EditPatientView;
 import ch.bfh.red.ui.views.ListPatientView;
+import ch.bfh.red.ui.views.SearchBean.PatientSearchBean;
+import com.vaadin.flow.component.UI;
 
 public class PatientPresenter implements EditPatientView.EditPatientViewListener, ListPatientView.ListPatientViewListener {
     EditPatientView editPatientView;
     ListPatientView listPatientView;
 
 
-    public PatientPresenter(EditPatientView editPatientView){
+    public PatientPresenter(EditPatientView editPatientView) {
         this.editPatientView = editPatientView;
         editPatientView.addListener(this);
     }
 
-    public PatientPresenter(ListPatientView listPatientView){
+    public PatientPresenter(ListPatientView listPatientView) {
         this.listPatientView = listPatientView;
         listPatientView.addListener(this);
     }
 
+    @Override
+    public void search(PatientSearchBean patientSearchBean) {
+        System.out.println("Vorname: " + patientSearchBean.getFirstName());
+        System.out.println("Nachname: " + patientSearchBean.getLastName());
+        System.out.println("Strasse: " + patientSearchBean.getStreet());
+        System.out.println("Hausnr: " + patientSearchBean.getStreetNr());
+        System.out.println("PLZ: " + patientSearchBean.getPostalCode());
+        System.out.println("Stadt: " + patientSearchBean.getCity());
+    }
+
+    @Override
+    public void onPatientClick(String param) {
+        UI.getCurrent().navigate(EditPatientView.class, param);
+    }
 }
