c7af3bbd5ea9808613084844499491ad976a7c9f
Sophie Haug <sophie.haug@hotmail.com>
2019-05-19 21:11:02 +0200

    Removing superfluous DateToStringEncoder

--- MhcPms/src/main/java/ch/bfh/red/ui/encoders/DateToStringEncoder.java
+++ MhcPms/src/main/java/ch/bfh/red/ui/encoders/DateToStringEncoder.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package ch.bfh.red.ui.encoders;
 
 import com.vaadin.flow.templatemodel.ModelEncoder;
@@ -30,39 +29,4 @@ public class DateToStringEncoder
     }
 
 }
-=======
-package ch.bfh.red.ui.encoders;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import com.vaadin.flow.templatemodel.ModelEncoder;
-
-/**
- * Converts between DateTime-objects and their String-representations
- */
-
-public class DateToStringEncoder
-        implements ModelEncoder<Date, String> {
-
-    public static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("MM/dd/yyyy");;
-
-    @Override
-    public Date decode(String presentationValue) {
-        try {
-			return  SIMPLE_DATE_FORMAT.parse(presentationValue);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-    }
-
-    @Override
-    public String encode(Date modelValue) {
-        return modelValue == null ? null : SIMPLE_DATE_FORMAT.format(modelValue);
-    }
-
-}
->>>>>>> implementing ExpositionView (Issue Nr. 6)
