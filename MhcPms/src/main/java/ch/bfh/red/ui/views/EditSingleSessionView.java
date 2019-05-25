c8dd83274bc4673ea4e10461f3ec87ee23379710
Marc Hutzli <m.hutzli@hotmail.com>
2019-05-25 18:49:58 +0200

    added ids to textFields.

--- MhcPms/src/main/java/ch/bfh/red/ui/views/EditSingleSessionView.java
+++ MhcPms/src/main/java/ch/bfh/red/ui/views/EditSingleSessionView.java
@@ -5,10 +5,13 @@ import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.polymertemplate.Id;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.templatemodel.TemplateModel;
 
 import ch.bfh.red.MainLayout;
+import ch.bfh.red.backend.models.SingleSession;
 
 @Route(value = "editSingleSession", layout = MainLayout.class)
 @Tag("editsinglesession-element")
@@ -20,11 +23,12 @@ public class EditSingleSessionView extends PolymerTemplate<EditSingleSessionView
     
     @Id("header")
     private H2 header;
-
+    
+    @Id
+    private TextField therapist; 
+    
     public EditSingleSessionView() {
     	 header.setText("Edit Single Session");
-    	
-    	
     }
     
     public interface EditSingleSessionListener {
@@ -33,6 +37,9 @@ public class EditSingleSessionView extends PolymerTemplate<EditSingleSessionView
     
     public interface EditSingleSessionModel extends TemplateModel {
         
+    	
+    	void setSingleSession(SingleSession singleSession);
+    	
     }
 
     @Override
